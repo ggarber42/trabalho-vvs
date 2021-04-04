04376ca89d1dbc84bb787c49a0040104ac2ef8c7
ggarber42 <ggarber@protonmail.com>
2021-04-04 18:22:56 -0300

    added selenium tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 
+
+# from django_selenium_clean import selenium, SeleniumTestCase, PageElement
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.firefox.options import Options
@@ -29,29 +32,29 @@ class TodoModelTest(TestCase):
         expected_object_completed= todo.completed
         self.assertEquals(expected_object_completed, False)
 
-# class TestUI(StaticLiveServerTestCase):
+class TestUI(StaticLiveServerTestCase):
 
-#     @classmethod
-#     def setUpClass(cls):
-#         super().setUpClass()
-#         options = Options()
-#         options.headless = True 
-#         cls.selenium = WebDriver(options=options,executable_path=r'./geckodriver.exe')
-#         cls.selenium.implicitly_wait(10)
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = Options()
+        options.headless = True 
+        cls.selenium = WebDriver(options=options,executable_path=r'./geckodriver.exe')
+        cls.selenium.implicitly_wait(10)
 
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.selenium.quit()
-#         super().tearDownClass()
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
 
-#     def test_crud(self):
-#         self.selenium.get("http://localhost:8000/")
-#         self.selenium.set_window_size(915, 472)
-#         self.selenium.find_element(By.ID, "__BVID__7").click()
-#         self.selenium.find_element(By.ID, "__BVID__7").send_keys("Teste")
-#         self.selenium.find_element(By.CSS_SELECTOR, "form").click()
-#         self.selenium.find_element(By.CSS_SELECTOR, ".btn-primary").click()
-#         self.selenium.find_element(By.CSS_SELECTOR, ".list-group:nth-child(3) input").click()
-#         self.selenium.find_element(By.CSS_SELECTOR, ".list-group:nth-child(3) > .list-group-item").click()
-#         self.selenium.find_element(By.CSS_SELECTOR, ".list-group:nth-child(3) input").click()
-#         self.selenium.find_element(By.CSS_SELECTOR, ".list-group:nth-child(3) .btn").click()
+    def test_crud(self):
+        self.selenium.get("http://localhost:8000/")
+        self.selenium.set_window_size(915, 472)
+        self.selenium.find_element(By.ID, "__BVID__7").click()
+        self.selenium.find_element(By.ID, "__BVID__7").send_keys("Teste")
+        self.selenium.find_element(By.CSS_SELECTOR, "form").click()
+        self.selenium.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+        self.selenium.find_element(By.CSS_SELECTOR, ".list-group:nth-child(3) input").click()
+        self.selenium.find_element(By.CSS_SELECTOR, ".list-group:nth-child(3) > .list-group-item").click()
+        self.selenium.find_element(By.CSS_SELECTOR, ".list-group:nth-child(3) input").click()
+        self.selenium.find_element(By.CSS_SELECTOR, ".list-group:nth-child(3) .btn").click()
