b1379756f8c5fbe0e330dee30ef60f549100b89a
ggarber42 <ggarber@protonmail.com>
2021-04-06 21:01:17 -0300

    apresetacao 2

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -68,7 +68,7 @@ class TestUI(StaticLiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         options = Options()
-        options.headless = True 
+        # options.headless = True 
         options.headless = False
         cls.selenium = WebDriver(options=options,executable_path=r'./geckodriver.exe')
         cls.selenium.implicitly_wait(10)
